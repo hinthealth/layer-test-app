0b84d82fe05b98d25de8eff23fc5a9658bf1953d
adrian-gomez <adri4n.steam@gmail.com>
2025-05-08 13:01:42 -0300

    Add layer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import './App.css';
+import "@layerfi/components/dist/index.css"
+
+import { LayerProvider, BankTransactions, ProfitAndLoss } from "@layerfi/components";
 
 export function App() {
+  const businessId = "<businessId>";
+  const environment = "sandbox";
+  const businessAccessToken = "<businessAccessToken>";
+
   return (
     <>
       <h1>Parcel React App</h1>
-      <p>Edit <code>src/App.tsx</code> to get started!</p>
+
+      <LayerProvider
+        businessId={businessId}
+        environment={environment}
+        businessAccessToken={businessAccessToken}
+      >
+      <BankTransactions />
+
+      <ProfitAndLoss>
+        <div className='profit-and-loss__chart-container'>
+          <ProfitAndLoss.Chart />
+        </div>
+        <ProfitAndLoss.Summaries />
+        <div className='profit-and-loss__date-picker-container'>
+          <ProfitAndLoss.DatePicker />
+        </div>
+        <ProfitAndLoss.Table />
+      </ProfitAndLoss>
+    </LayerProvider>
     </>
   );
 }
